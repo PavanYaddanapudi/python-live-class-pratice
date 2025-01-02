39eec1e9636d8204de7f118697a64b58eed3836f
PavanYaddanapudi <pavanyaddanapudi323@gmail.com>
2025-01-02 18:13:01 +0530

    Add files via upload

---
+++ first program data types.py
@@ -0,0 +1,8 @@
+age=22
+fname="pavan"
+lname="yaddanapudi"
+email="pavanyaddanapudi323@gmail.com"
+print("fullname",fname + " " + lname)
+print("index of @ in email", email.find("@"))#task-1
+print("print .com from the email:" , email[-4:])#task-2
+print("print gmail word from email:",email[20:25])#task-3
