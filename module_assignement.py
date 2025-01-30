9f1032cfd52f085c642df374602bfe5e8c1b7292
PavanYaddanapudi <pavanyaddanapudi323@gmail.com>
2025-01-30 15:44:41 +0530

    Update module_assignement.py

--- module_assignement.py
+++ module_assignement.py
@@ -28,3 +28,33 @@ def generate_otp():
     return random_otp
 otp = generate_otp()
 print("random otp of six digit : ", otp)
+#Exercise - 4
+def random_character():
+    str = input("enter a string : ")
+    if str.isalpha():
+        random_char = random.choice(str)
+        return random_char
+random_string = random_character()
+print(random_string)
+
+#Exercise - 5
+import string
+def generate_randomstring():
+    return ''.join(random.choices(string.ascii_letters + string.digits, k=5))
+random_str = generate_randomstring()
+print("random string : " , random_str)
+
+
+#Exercise - 6
+import random
+import string
+
+def generate_random_password():
+    password = [random.choice(string.ascii_uppercase) for _ in range(2)]
+    password.append(random.choice(string.digits))
+    password.append(random.choice(string.punctuation))
+    password += random.choices(string.ascii_lowercase, k=6)
+    return ''.join(password)
+password = generate_random_password()
+print("Generated Password:", password)
+
